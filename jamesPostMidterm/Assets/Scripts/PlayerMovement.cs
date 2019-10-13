651380f5d751ce2987f17cf7c8fb4d8105b01cbc
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-13 10:31:14 -0500

    got player movement working, working on shrinking dustballs

---
+++ jamesPostMidterm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //private variables
+    [SerializeField] float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //stores the return value of the horizontal input axis (by default, the horzizontal axis is controled by the "a" and "d" keys
+        horizontalInput = Input.GetAxis("Horizontal");
+        //stores the return value of the vertical input axis (by default, the vertical axis is controled by the "w" and "s" keys
+        forwardInput = Input.GetAxis("Vertical");
+
+        //We'll move the vehicle forward with the "w" key, and backward with the "s" key
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotate the player left with the "a" key and right with the "d" key
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
---
+++ jamesPostMidterm/Assets/Scripts/ShrinkAndDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkAndDestroy : MonoBehaviour
+{
+    //if object collides with "Player" then shrink over time and destroy self
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("I've been hit by the player!");
+        }
+    }
+}
