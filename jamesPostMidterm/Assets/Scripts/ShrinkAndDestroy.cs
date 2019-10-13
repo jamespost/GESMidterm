98278a95ac6b0de9d18216e3daadf0daf43331ce
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-13 11:27:23 -0500

    got shrinking and destroying working on dustball

--- jamesPostMidterm/Assets/Scripts/ShrinkAndDestroy.cs
+++ jamesPostMidterm/Assets/Scripts/ShrinkAndDestroy.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class ShrinkAndDestroy : MonoBehaviour
 {
+    //amount to scale transform by
+    Vector3 scaleVector;
+    [SerializeField] float scaleFactor = 0.25f;
+
+    private void Start()
+    {
+        //initialize scaleFactor
+        scaleVector = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+    }
     //if object collides with "Player" then shrink over time and destroy self
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            //print a debug message
             Debug.Log("I've been hit by the player!");
+            //shrink myself
+            gameObject.transform.localScale -= scaleVector;
+            //print a debug message
+            Debug.Log(gameObject.transform.localScale);
+            //
+            if (gameObject.transform.localScale.Equals(new Vector3(0f, 0f, 0f)))
+            {
+                Destroy(gameObject);
+                Debug.Log("Object destroyed");
+            }
+            
         }
     }
 }
