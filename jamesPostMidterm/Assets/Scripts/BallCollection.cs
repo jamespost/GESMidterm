8520847549a3139bd0f93eb93ffa2df002e544a8
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-19 17:09:39 -0500

    created ballsCollected text element in canvas
    also added this text as the player's text requried for BallCollection script

--- jamesPostMidterm/Assets/Scripts/BallCollection.cs
+++ jamesPostMidterm/Assets/Scripts/BallCollection.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallCollection : MonoBehaviour
 {
     //a variable to store the number of balls collected
     public int ballsCollected = 0;
+    //a field to alter UI text
+    public Text ballCollectedText;
 }
