71b70dacdb71e7da5575d5f5eca8f75be3430844
jamespost <32137126+jamespost@users.noreply.github.com>
2019-11-02 09:08:28 -0500

    Update BallSpawner.cs

--- jamesPostMidterm/Assets/Scripts/BallSpawner.cs
+++ jamesPostMidterm/Assets/Scripts/BallSpawner.cs
@@ -20,8 +20,9 @@ public class BallSpawner : MonoBehaviour
     {        
         int randX = Random.Range(-19, 19);
         int randZ = Random.Range(-19, 19);
-        //x bounds (19,-19) y bounds (0,0) z bounds (19,-19)
+        //instantiate the objectToSpawn at a random location within the bounds of the level
         Instantiate(objectToSpawn, new Vector3(randX, 0.5f, randZ), Quaternion.identity);
+        //set the objectToSpawns ballCollection component to the player's ball collection compononent
         objectToSpawn.GetComponent<BallCollision>().ballCollection = GameObject.FindGameObjectWithTag("Player").GetComponent<BallCollection>();
     }
 
