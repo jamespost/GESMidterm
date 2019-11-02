42ed128686f7c4f8c710d829d31027f6618ba985
jamespost <32137126+jamespost@users.noreply.github.com>
2019-11-02 09:32:21 -0500

    implemented player jump mechanic

--- jamesPostMidterm/Assets/Scripts/PlayerMovement.cs
+++ jamesPostMidterm/Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    Rigidbody playerRb;
+    [SerializeField] float jumpForce;
+    private bool isOnGround = true;
 
+    private void Start()
+    {
+        //initialze variables
+        playerRb = GetComponent<Rigidbody>();
+        jumpForce = 10;
+        //debug
+        //Debug.Log(GetComponent<Rigidbody>());
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
+        /*normal movement*/
         //stores the return value of the horizontal input axis (by default, the horzizontal axis is controled by the "a" and "d" keys
         horizontalInput = Input.GetAxis("Horizontal");
         //stores the return value of the vertical input axis (by default, the vertical axis is controled by the "w" and "s" keys
@@ -24,5 +37,19 @@ public class PlayerMovement : MonoBehaviour
         //rotate the player left with the "a" key and right with the "d" key
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
+        /*jumping*/
+        //if the player presses the spacebar
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true )
+        {
+            //add force to playerRb (make the object jump up)
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //debug
+            Debug.Log("jump!");
+
+            isOnGround = false;
+            
+            //play the jump sound
+            
+        }
     }
 }
