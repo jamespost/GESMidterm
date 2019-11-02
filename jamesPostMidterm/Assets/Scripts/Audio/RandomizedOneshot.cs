7d26404f629a7231afc4dc11a667946f4cda9c50
jamespost <32137126+jamespost@users.noreply.github.com>
2019-11-02 15:43:29 -0500

    restructured asset folder organization

---
+++ jamesPostMidterm/Assets/Scripts/Audio/RandomizedOneshot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizedOneshot : MonoBehaviour
+{
+    //a class that plays a sound back as a one shot with user defined random pitch and volume parameters
+    //fields
+
+}
