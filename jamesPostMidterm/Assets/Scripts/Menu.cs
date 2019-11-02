8770670f25f05e493bce6eb0be34127ba0ff3283
jamespost <32137126+jamespost@users.noreply.github.com>
2019-11-02 11:44:04 -0500

    got main menu "play" button working
    correctly loads the game level on button click

---
+++ jamesPostMidterm/Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject creditsMenu;
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+
+    public void ShowCreditsMenu()
+    {
+        creditsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ jamesPostMidterm/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //load the game scene
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game Level");
+    }
+}
